858d62904eed2da5b621b40b0a41636032bfb500
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-03-13 15:27:07 +0000

    Initial commit

---
+++ src/test/java/uk/ac/leedsbeckett/hmm/student_portal/StudentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.leedsbeckett.hmm.student_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
