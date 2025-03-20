a34de793c81bf3950e6fdd5106b7f5172bc5e694
hponemyatmin <hponemyatmin@my.jcu.edu.au>
2025-03-20 02:16:57 +0000

    Add Course Repository

--- src/main/java/uk/ac/leedsbeckett/hmm/student_portal/repositories/CourseRepository.java
+++ src/main/java/uk/ac/leedsbeckett/hmm/student_portal/repositories/CourseRepository.java
@@ -3,6 +3,8 @@ package uk.ac.leedsbeckett.hmm.student_portal.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.ac.leedsbeckett.hmm.student_portal.entities.Course;
 
-public interface CourseRepository extends JpaRepository<Course, Long> {
+import java.util.Optional;
 
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    Optional<Course> findCourseByTitle(String title);
 }
